cb240ae7ed87cca7f54614a093f9ae8f580c643e
Bryce DeSantis <potreader@hotmail.com>
2023-10-02 13:53:56 -0400

    Create functions for buttons

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+let firstNum;
+let operator;
+let secondNum;
+
 function add(num1, num2) {
     return num1 + num2
 }
@@ -14,4 +18,17 @@ function divide(num1, num2) {
     return num1 / num2
 }
 
-console.log(divide(10,5))
+function operate(first, op, second) {
+    switch(op) {
+        case '+':
+            return add(first, second)
+        case '-':
+            return subtract(first, second)
+        case '*':
+            return multiply(first, second)
+        case '/':
+            return divide(first, second)
+    }
+}
+
+console.log(operate(5, '*', 6))
