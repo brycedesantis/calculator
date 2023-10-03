2c31df354d5fa989733f2779315b59fe6360a2cf
Bryce DeSantis <potreader@hotmail.com>
2023-10-03 15:27:40 -0400

    Add functionality to calculator

--- script.js
+++ script.js
@@ -1,19 +1,65 @@
 let firstNum = '';
-let operator = null;
 let secondNum = '';
+let operator = null;
+let selfClear = false
 
 const numberBtns = document.querySelectorAll('.digits')
+const operatorBtns = document.querySelectorAll('.operator')
+const equals = document.querySelector('.equal')
+const clear = document.querySelector('.clear')
+
 const mainDisplay = document.querySelector('.input')
+const secondDisplay = document.querySelector('.equation')
 
 numberBtns.forEach(button => 
     button.addEventListener('click', () => addNumber(button.textContent))
 )
 
+operatorBtns.forEach(button => 
+    button.addEventListener('click', () => performOperation(button.textContent))    
+)
+
+equals.addEventListener('click', total)
+
+clear.addEventListener('click', clearScreen)
+
 function addNumber(number) {
-    if (mainDisplay.textContent === '0'){
+    if (mainDisplay.textContent === '0' || selfClear){
         mainDisplay.textContent = ''
+        selfClear = false
     }
     mainDisplay.textContent += number
+}
+
+function performOperation(operatorBtn) {
+    if(operator !== null) total()
+    firstNum = mainDisplay.textContent
+    operator = operatorBtn
+    secondDisplay.textContent = `${firstNum} ${operator}`
+    selfClear = true
+}
+
+function total() {
+    if(operator === null || selfClear){
+        return
+    }
+
+    if (operator === '/' && mainDisplay.textContent === '0') {
+        mainDisplay.textContent = 'Can not divide by 0'
+        return
+    }
+    secondNum = mainDisplay.textContent
+    mainDisplay.textContent = operate(firstNum, operator, secondNum)
+    secondDisplay.textContent = `${firstNum} ${operator} ${secondNum} =`
+    operator = null
+}
+
+function clearScreen() {
+    mainDisplay.textContent = '0'
+    secondDisplay.textContent = ''
+    firstNum = ''
+    secondNum = ''
+    operator = null
 }
 
 function add(num1, num2) {
@@ -33,17 +79,16 @@ function divide(num1, num2) {
 }
 
 function operate(first, op, second) {
+    first = parseInt(first)
+    second = parseInt(second)
     switch(op) {
         case '+':
             return add(first, second)
         case '-':
             return subtract(first, second)
-        case '*':
+        case 'x':
             return multiply(first, second)
         case '/':
             return divide(first, second)
     }
 }
-
-console.log(operate(5, '*', 6))
-
